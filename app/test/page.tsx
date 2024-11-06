c2c4dde94d28c48c524370e57b8e761f83d5f7de
suhas sensei <suhas.ghosal2002@gmail.com>
2024-11-07 01:36:34 +0530

    v2.0

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -5,7 +5,7 @@ import useEmblaCarousel from 'embla-carousel-react'
 import EmblaCarousel from "@/components/EmblaCarousel";
 import ReactDOM from 'react-dom/client'
 import { EmblaOptionsType } from 'embla-carousel'
-import './globals.css';
+import '../globals.css';
 
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
 const SLIDE_COUNT = 4
